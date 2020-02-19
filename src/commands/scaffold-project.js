29cc4b6874ef407efd1678ac5f75ed55633cb110
Trevor Richardson <richardson.trev@gmail.com>
2020-02-18 21:30:52 -0800

    feat(scaffold-project): add some default config
    - added javascript language scaffolder
    - added myself as default copyright holder

--- src/commands/scaffold-project.js
+++ src/commands/scaffold-project.js
@@ -1,11 +1,15 @@
 const {Command} = require('@oclif/command');
+const {scaffold: scaffoldJavaScript} = require('@travi/javascript-scaffolder');
 const {scaffold} = require('@travi/project-scaffolder');
 
 class ScaffoldProject extends Command {
   async run() {
     this.log('Starting the scaffolder.');
 
-    scaffold().catch(err => {
+    scaffold({
+      languages: {JavaScript: scaffoldJavaScript},
+      overrides: {copyrightHolder: 'Trevor Richardson'}
+    }).catch(err => {
       console.error(err); // eslint-disable-line no-console
       process.exitCode = 1;
     });
