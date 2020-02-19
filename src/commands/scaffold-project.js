2ddd6bbd774fed01c8b44f6d217f826ee7878ee5
Trevor Richardson <richardson.trev@gmail.com>
2020-02-18 21:52:57 -0800

    feat(scaffold-project): add travis-ci as javascript CI target

--- src/commands/scaffold-project.js
+++ src/commands/scaffold-project.js
@@ -1,6 +1,7 @@
 const {Command} = require('@oclif/command');
 const {prompt: githubPrompt, scaffold: scaffoldGithub} = require('@travi/github-scaffolder');
 const {scaffold: scaffoldJavaScript} = require('@travi/javascript-scaffolder');
+const {scaffold: scaffoldTravisForJavaScript} = require('@travi/travis-scaffolder-javascript');
 const {scaffold} = require('@travi/project-scaffolder');
 
 class ScaffoldProject extends Command {
@@ -8,7 +9,12 @@ class ScaffoldProject extends Command {
     this.log('Starting the scaffolder.');
 
     scaffold({
-      languages: {JavaScript: scaffoldJavaScript},
+      languages: {
+        JavaScript: options => scaffoldJavaScript({
+          ...options,
+          ciServices: {Travis: {scaffolder: scaffoldTravisForJavaScript, public: true}}
+        })
+      },
       overrides: {copyrightHolder: 'Trevor Richardson'},
       vcsHosts: {
         GitHub: {scaffolder: scaffoldGithub, prompt: githubPrompt, public: true, private: true}
