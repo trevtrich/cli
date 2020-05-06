2409d64e39da2c0b5935b1cd2f0959a5051d768c
Trevor Richardson <richardson.trev@gmail.com>
2020-05-05 22:38:25 -0500

    feat(lift): dont require input for dependabot scaffolder
    - vcs info is now provided by lift itself

--- src/commands/lift.js
+++ src/commands/lift.js
@@ -1,6 +1,5 @@
 const {Command} = require('@oclif/command');
 const {lift} = require('@form8ion/lift');
-const cli = require('cli-ux').default;
 const {scaffold: scaffoldRenovate} = require('@form8ion/renovate-scaffolder');
 const {scaffold: scaffoldDependabot} = require('@form8ion/dependabot-scaffolder');
 const {removeGreenkeeper} = require('@form8ion/remove-greenkeeper');
@@ -11,12 +10,7 @@ class Lift extends Command {
 
     lift({
       scaffolders: {
-        Dependabot: async options => {
-          const owner = await cli.prompt('Who is the owner of the current repo?');
-          const name = await cli.prompt('What is the name of this repo?');
-
-          return scaffoldDependabot({...options, vcs: {owner, name}});
-        },
+        Dependabot: scaffoldDependabot,
         Renovate: scaffoldRenovate,
         'Remove Greenkeeper': removeGreenkeeper
       }
